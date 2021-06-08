26f8df6235513b6201ea3dd197b9244e03874bec
roozbehdn <rdehghan@stanford.edu>
2021-06-08 13:55:44 -0700

    update postprocessing for SS2

--- scripts/post_processing.R
+++ scripts/post_processing.R
@@ -2,28 +2,19 @@ library(data.table)
 
 args <- commandArgs(trailingOnly = TRUE)
 directory = args[1]
-run = args[2] 
-
-consolidated_GLM_file = paste(directory,run,"_GLM_outputs_consolidated.txt",sep="")
-SICILIAN_junctions_file = paste(directory,run,".tsv",sep="")
-is.SE = 1
+run = args[2]
+is.SE = args[3]
 
 #########  read in input files #################
+consolidated_GLM_file = paste(directory,run,"_GLM_outputs_consolidated.txt",sep="")
 consolidated_GLM = fread(consolidated_GLM_file,sep="\t",header=TRUE)
-SICILIAN_junctions = fread(SICILIAN_junctions_file,sep="\t",header = TRUE)
+
+if (is.SE==1){
+  SICILIAN_junctions_file = paste(directory,run,".tsv",sep="")
+  SICILIAN_junctions = fread(SICILIAN_junctions_file,sep="\t",header = TRUE)
+  SICILIAN_junctions[,c("frac_passed_samples","emp.p_glmnet_constrained_median","called"):=NULL]
+}
 ###############################################
-print(names(consolidated_GLM))
-SICILIAN_junctions[,c("frac_passed_samples","emp.p_glmnet_constrained_median","called"):=NULL]
-
-#SICILIAN_junctions = data.table()
-
-#list_files = c("Krasnow_COVID_10x_pilot4_201214S2.tsv","Krasnow_COVID_10x_pilot4_201214S3.tsv","Krasnow_COVID_10x_pilot4_201214S4.tsv","Krasnow_COVID_10x_pilot4_201214S5.tsv","Krasnow_COVID_10x_pilot4_201214S6.tsv","Krasnow_COVID_10x_pilot4_201214S7.tsv","Krasnow_COVID_10x_pilot4_201214S8.tsv","Krasnow_COVID_10x_pilot4_201214S9.tsv","Krasnow_COVID_10x_pilot4_201214S10.tsv")
-
-#for (counter in 1: length(list_files)){
-# file_name = list_files[counter]
-# report = fread(paste(SICILIAN_junctions_file,file_name, sep = ""), sep = "\t",header=TRUE)
-# SICILIAN_junctions = rbind(SICILIAN_junctions,report)
-#}
 
 consolidated_GLM[,passed_tot:=0]
 consolidated_GLM[(postprocess_passed==1) & (frac_genomic_reads<0.1),passed_tot:=1]
@@ -39,13 +30,26 @@ if (is.SE==1){
   consolidated_GLM_uniq[,called:=0]
   consolidated_GLM_uniq[(emp.p_glmnet_constrained_median<0.1) & ((frac_passed_samples>0.5) | (frac_passed_samples==0.5 & sample_num>2)),called:=1]
   consolidated_GLM_uniq = consolidated_GLM_uniq[!duplicated(refName_newR1)]
-  SICILIAN_junctions = merge(SICILIAN_junctions,consolidated_GLM_uniq[,list(refName_newR1,frac_passed_samples,emp.p_glmnet_constrained_median,called)],by.x="refName_newR1",by.y="refName_newR1",all.x=TRUE,all.y=FALSE)
+  consolidated_GLM = merge(SICILIAN_junctions,consolidated_GLM_uniq[,list(refName_newR1,frac_passed_samples,emp.p_glmnet_constrained_median,called)],by.x="refName_newR1",by.y="refName_newR1",all.x=TRUE,all.y=FALSE)
 } else{
-  consolidated_GLM_uniq = consolidated_GLM[!duplicated(refName_newR1),list(refName_newR1,frac_passed_samples,sample_num,emp.p_glmnet_corrected_constrained_median)]
-  consolidated_GLM_uniq[,called:=0]
-  consolidated_GLM_uniq[(emp.p_glmnet_corrected_constrained_median<0.1) & ((frac_passed_samples>0.5) | (frac_passed_samples==0.5 & sample_num>2)),called:=1]
-  consolidated_GLM_uniq = consolidated_GLM_uniq[!duplicated(refName_newR1)]
-  SICILIAN_junctions = merge(SICILIAN_junctions,consolidated_GLM_uniq[,list(refName_newR1,frac_passed_samples,emp.p_glmnet_corrected_constrained_median,called)],by.x="refName_newR1",by.y="refName_newR1",all.x=TRUE,all.y=FALSE)
+  setnames(consolidated_GLM,"sample_name","cell")
+  consolidated_GLM[,channel:=cell]
+  consolidated_GLM[,called:=0]
+  consolidated_GLM[(emp.p_glmnet_corrected_constrained_median<0.1) & ((frac_passed_samples>0.5) | (frac_passed_samples==0.5 & sample_num>2)),called:=1]
+  
+  consolidated_GLM[, chrR1A:=strsplit(refName_newR1, split = ":", fixed = TRUE)[[1]][1], by = refName_newR1]
+  consolidated_GLM[, chrR1B:=strsplit(refName_newR1, split = "[:|]")[[1]][5], by = refName_newR1]
+  consolidated_GLM[, juncPosR1A:=as.integer(strsplit(refName_newR1, split = ":", fixed = TRUE)[[1]][3]), by = refName_newR1]
+  consolidated_GLM[, juncPosR1B:=as.integer(strsplit(refName_newR1, split = ":", fixed = TRUE)[[1]][6]), by = refName_newR1]
+  consolidated_GLM[, geneR1A_uniq:=strsplit(refName_newR1, split = ":", fixed = TRUE)[[1]][2], by = refName_newR1]
+  
+  options(scipen = 999)
+  round.bin = 100000
+  consolidated_GLM[geneR1A_uniq=="",geneR1A_uniq:="unknown"]
+  consolidated_GLM[geneR1A_uniq=="unknown", binR1A:=round(juncPosR1A/round.bin)*round.bin]
+  consolidated_GLM[geneR1A_uniq=="unknown",geneR1A_uniq:=paste("unknown",chrR1A,binR1A,sep = "_")]
+  consolidated_GLM[, binR1A:= NULL]
+  consolidated_GLM[, binR1B:= NULL]
 }
 
-write.table(SICILIAN_junctions,paste(directory,"/",run,"_with_postprocessing.txt",sep=""),row.names=FALSE,sep="\t",quote=FALSE)
+write.table(consolidated_GLM,paste(directory,"/",run,"_with_postprocessing.txt",sep=""),row.names=FALSE,sep="\t",quote=FALSE)
